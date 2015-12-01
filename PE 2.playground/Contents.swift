4d43b613569156baa4c872f94ea0391c766d9a16
Elvis He <elvis10130903@Elviss-MBP.telecom>
2015-12-01 14:25:54 +1300

    Func of Fibonacci sequence

---
+++ PE 2.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var a = 1
+
+var b = 2
+
+var c = a + b
+
+while c < 10 {
+    
+  
+    a = b
+    b = c
+    c = a + b
+    
+    
+}
+ print(b)
